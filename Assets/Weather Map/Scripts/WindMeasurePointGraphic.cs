94a4e35bbfa8ae7a7f758a30f20ae53a506f4156
Kosmik <malpa.w.klapkach@gmail.com>
2024-05-31 12:23:03 +0200

    Include wind measure point rotation in graphics rotation

--- Assets/Weather Map/Scripts/WindMeasurePointGraphic.cs
+++ Assets/Weather Map/Scripts/WindMeasurePointGraphic.cs
@@ -6,7 +6,7 @@ public class WindMeasurePointGraphic : MonoBehaviour
     private WindMeasurePoint measurePoint;
     [SerializeField]
     private Transform visual;
-        
+
     private void Reset()
     {
         measurePoint = GetComponentInParent<WindMeasurePoint>();
@@ -25,8 +25,11 @@ public class WindMeasurePointGraphic : MonoBehaviour
     [ContextMenu("Refresh Graphic")]
     private void RefreshGraphic()
     {
+        var measurePointForward = measurePoint.transform.forward;
         var windVelocity = measurePoint.WindVelocity;
-        float angle = Vector2.SignedAngle(Vector3.up, windVelocity);
-        visual.localEulerAngles = new Vector3(0, 0, angle);
+        float angle = Vector2.SignedAngle(Vector2.up, windVelocity);
+        float pointAngle = Vector2.SignedAngle(Vector2.up, new Vector2(measurePointForward.x, measurePointForward.z));
+
+        visual.localEulerAngles = new Vector3(0, 0, angle - pointAngle);
     }
 }
